8ad7a570c56db2a674aebda17417fe32af5b223f
RuLeZzZ1128 <58522820+RuLeZzZ1128@users.noreply.github.com>
2019-12-05 17:49:16 +0300

    Update task_1_1.py

--- task_1_1.py
+++ task_1_1.py
@@ -1,5 +1,5 @@
 # Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
-#https://drive.google.com/open?id=1Ra-VUBF5ftKQ9LOv8dPZ4jgPIowuD4Rr
+#https://drive.google.com/open?id=1q-mWRzF8zqPoq5o1FmqUIeqdl7rG3LUF
 a=int(input('Введите трехзначное число:'))
 a1=a//100
 a2=(a-a1*100)//10
